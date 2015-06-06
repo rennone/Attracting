2b435c9ca5259e1d05329676bd2a93ca456d1df9
Ren Mizutani <nightfall-immortal-longview@live.jp>
2015-06-07 01:17:53 +0900

    係数を外部からいじれるようにした

--- MagneticForce/Form1.Designer.cs
+++ MagneticForce/Form1.Designer.cs
@@ -31,10 +31,30 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.randomPlacementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.particleNumLabel = new System.Windows.Forms.Label();
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.minKill = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.soulAccel = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.soulSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maxKill = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.attractivePower = new System.Windows.Forms.NumericUpDown();
+            this.allDispersalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            this.Canvas.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minKill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soulAccel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soulSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxKill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attractivePower)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -46,51 +66,242 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.startToolStripMenuItem,
-            this.randomPlacementToolStripMenuItem});
+            this.randomPlacementToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.allDispersalToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(978, 26);
+            this.menuStrip1.Size = new System.Drawing.Size(1204, 26);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // startToolStripMenuItem
-            // 
-            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
-            this.startToolStripMenuItem.Size = new System.Drawing.Size(50, 22);
-            this.startToolStripMenuItem.Text = "Start";
-            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
             // 
             // randomPlacementToolStripMenuItem
             // 
             this.randomPlacementToolStripMenuItem.Name = "randomPlacementToolStripMenuItem";
-            this.randomPlacementToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
-            this.randomPlacementToolStripMenuItem.Text = "RandomPlacement";
+            this.randomPlacementToolStripMenuItem.Size = new System.Drawing.Size(73, 22);
+            this.randomPlacementToolStripMenuItem.Text = "Dispersal";
             this.randomPlacementToolStripMenuItem.Click += new System.EventHandler(this.randomPlacementToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(47, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
             // 
             // particleNumLabel
             // 
             this.particleNumLabel.AutoSize = true;
-            this.particleNumLabel.Location = new System.Drawing.Point(922, 9);
+            this.particleNumLabel.Location = new System.Drawing.Point(1038, 8);
             this.particleNumLabel.Name = "particleNumLabel";
             this.particleNumLabel.Size = new System.Drawing.Size(35, 12);
             this.particleNumLabel.TabIndex = 1;
             this.particleNumLabel.Text = "label1";
             // 
+            // Canvas
+            // 
+            this.Canvas.Controls.Add(this.particleNumLabel);
+            this.Canvas.Location = new System.Drawing.Point(2, 24);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1079, 702);
+            this.Canvas.TabIndex = 2;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseClick);
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.Controls.Add(this.minKill);
+            this.controlPanel.Controls.Add(this.label5);
+            this.controlPanel.Controls.Add(this.soulAccel);
+            this.controlPanel.Controls.Add(this.label4);
+            this.controlPanel.Controls.Add(this.soulSpeed);
+            this.controlPanel.Controls.Add(this.label3);
+            this.controlPanel.Controls.Add(this.label2);
+            this.controlPanel.Controls.Add(this.maxKill);
+            this.controlPanel.Controls.Add(this.label1);
+            this.controlPanel.Controls.Add(this.attractivePower);
+            this.controlPanel.Location = new System.Drawing.Point(1088, 29);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(116, 697);
+            this.controlPanel.TabIndex = 3;
+            // 
+            // minKill
+            // 
+            this.minKill.Location = new System.Drawing.Point(57, 77);
+            this.minKill.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minKill.Name = "minKill";
+            this.minKill.Size = new System.Drawing.Size(56, 19);
+            this.minKill.TabIndex = 9;
+            this.minKill.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.minKill.ValueChanged += new System.EventHandler(this.minKill_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "最少キル";
+            // 
+            // soulAccel
+            // 
+            this.soulAccel.Location = new System.Drawing.Point(42, 234);
+            this.soulAccel.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.soulAccel.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.soulAccel.Name = "soulAccel";
+            this.soulAccel.Size = new System.Drawing.Size(71, 19);
+            this.soulAccel.TabIndex = 7;
+            this.soulAccel.Value = new decimal(new int[] {
+            800,
+            0,
+            0,
+            0});
+            this.soulAccel.ValueChanged += new System.EventHandler(this.soulAccel_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "魂加速度";
+            // 
+            // soulSpeed
+            // 
+            this.soulSpeed.Location = new System.Drawing.Point(42, 166);
+            this.soulSpeed.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.soulSpeed.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.soulSpeed.Name = "soulSpeed";
+            this.soulSpeed.Size = new System.Drawing.Size(71, 19);
+            this.soulSpeed.TabIndex = 5;
+            this.soulSpeed.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "魂速度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "最大キル";
+            // 
+            // maxKill
+            // 
+            this.maxKill.Location = new System.Drawing.Point(57, 115);
+            this.maxKill.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.maxKill.Name = "maxKill";
+            this.maxKill.Size = new System.Drawing.Size(56, 19);
+            this.maxKill.TabIndex = 2;
+            this.maxKill.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.maxKill.ValueChanged += new System.EventHandler(this.maxKill_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "引力";
+            // 
+            // attractivePower
+            // 
+            this.attractivePower.Location = new System.Drawing.Point(57, 17);
+            this.attractivePower.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.attractivePower.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.attractivePower.Name = "attractivePower";
+            this.attractivePower.Size = new System.Drawing.Size(56, 19);
+            this.attractivePower.TabIndex = 0;
+            this.attractivePower.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // allDispersalToolStripMenuItem
+            // 
+            this.allDispersalToolStripMenuItem.Name = "allDispersalToolStripMenuItem";
+            this.allDispersalToolStripMenuItem.Size = new System.Drawing.Size(87, 22);
+            this.allDispersalToolStripMenuItem.Text = "AllDispersal";
+            this.allDispersalToolStripMenuItem.Click += new System.EventHandler(this.allDispersalToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 553);
-            this.Controls.Add(this.particleNumLabel);
+            this.ClientSize = new System.Drawing.Size(1204, 737);
+            this.Controls.Add(this.controlPanel);
+            this.Controls.Add(this.Canvas);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.Canvas.ResumeLayout(false);
+            this.Canvas.PerformLayout();
+            this.controlPanel.ResumeLayout(false);
+            this.controlPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minKill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soulAccel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.soulSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxKill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.attractivePower)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,9 +311,22 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem randomPlacementToolStripMenuItem;
         private System.Windows.Forms.Label particleNumLabel;
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Panel controlPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown attractivePower;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown soulSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown maxKill;
+        private System.Windows.Forms.NumericUpDown soulAccel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown minKill;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem allDispersalToolStripMenuItem;
     }
 }
 
--- MagneticForce/Form1.cs
+++ MagneticForce/Form1.cs
@@ -40,15 +40,15 @@ namespace MagneticForce
             public Rectangle LeftRect { get { return new Rectangle(rect.Left, rect.Top, rect.Width / 2, rect.Height); } }
             public Rectangle RightRect { get { return new Rectangle(rect.Left + rect.Width/2, rect.Top, rect.Width / 2, rect.Height); } }
 
-            public float M { get; private set; }
+            public float M { get; set; }
 
-            public Magnet(Rectangle a, float m = 90) 
+            public Magnet(Rectangle a, float m) 
             {
                 rect = a;
                 M = m;
             }
 
-            public PointF GetForce(Particle p)
+            public PointF GetForce(Soul p)
             {
                 var attraction = GetVector(p, Left);
                 var repulsion = GetVector(p, Right, false);
@@ -56,7 +56,7 @@ namespace MagneticForce
                 return Physics.Add(attraction, repulsion);
             }
 
-            public PointF GetVector(Particle p, PointF p2, bool attract = true)
+            public PointF GetVector(Soul p, PointF p2, bool attract = true)
             {
                 var v = Physics.Sub(p2, p.pos);
                 var lenSqr = Physics.LengthSqr(v);
@@ -78,7 +78,7 @@ namespace MagneticForce
 
         }
 
-        class Particle
+        class Soul
         {
             //位置ベクトル
             public PointF pos { get; private set; }
@@ -93,22 +93,21 @@ namespace MagneticForce
             public float elapsedTime { get; private set; }
 
             //速度
-            float speed = 600.0f;
+            public float speed = 600.0f;
 
             //加速度
-            const float accel = 800.0f;
+            public static float accel = 800.0f;
 
             //最初のポップアニメーションの時間
             const float popUpTime = 0.15f;
 
             const float popStopTime = 0.1f;
 
-            bool flg;
 
-            public Particle(PointF a, bool f, float w = 1.0f)
+            public Soul(PointF a, float s, float w = 1.0f)
             {
                 pos = a;
-                flg = f;
+                speed = s;
                 weight = w;
                 vec = new PointF();
                 elapsedTime = 0;
@@ -120,7 +119,9 @@ namespace MagneticForce
 
                 if(elapsedTime < popUpTime + popStopTime)
                 {
-                    pos = new PointF(pos.X, pos.Y - speed * deltaTime * (float)Math.Cos(Math.PI * 0.5 * elapsedTime / popUpTime) );
+                    //var move = (float)Math.Cos(Math.PI * 0.5 * elapsedTime / popUpTime);  //処理は重いが,非線形
+                    var move = -(float)Math.Pow( (elapsedTime - popUpTime) / popUpTime, 1); //処理は早いが線形
+                    pos = new PointF(pos.X, pos.Y - speed * deltaTime * move );
                     return;
                 }
 
@@ -138,90 +139,205 @@ namespace MagneticForce
         }
 
 
-        public bool Simulating { get; set; }
-        Rectangle Square(PointF p)
+        class SoulGroup
         {
-            var size = 10;
+            public List<Soul> particles { get; private set; }
+
+            public Color color { get; private set; }
+
+            public SoulGroup(List<Soul> list)
+            {
+                particles = list;
+
+                color = GetColor(particles.Count);
+            }
+
+            static Color GetColor(int particleNum)
+            {
+                int n = particleNum / 10;
+
+                int coef = 155;
+                return Color.FromArgb(255, coef * (n & 1), coef * ((n >> 1) & 1), coef * ((n >> 2) & 1));
+            }
+        }
+
+
+        //シミュレーション中
+        bool underSimulating = false;
+        public bool UnderSimulating 
+        { 
+            get{return underSimulating;}
+            set
+            {
+                underSimulating = value;
+                controlPanel.Visible = !underSimulating;
+                magBar.M = (float)attractivePower.Value;
+            }
+        }
+
+        Rectangle Square(PointF p, int size = 15)
+        {
             return new Rectangle((int)(p.X - size / 2), (int)(p.Y - size / 2), size, size);
         }
 
-        Magnet magBar = new Magnet(new Rectangle (300, 50, 800, 50));
+        Magnet magBar;
+
+        
+        List<SoulGroup> soulGroups = new List<SoulGroup>();
 
 
-        List<Particle> particles = new List<Particle>();
+        Rectangle windowRect = new Rectangle(1, 1, 960, 640);
 
         public Form1()
         {
             InitializeComponent();
-            Simulating = false;
+            magBar = new Magnet(new Rectangle(300, 50, 800, 50), (float)attractivePower.Value);
+            UnderSimulating = false;
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
             g.Clear(Color.White);
 
+            //磁石の描画
             g.FillRectangle(Brushes.Red, magBar.LeftRect);
             g.FillRectangle(Brushes.Blue, magBar.RightRect);
-            foreach(var p in particles)
+
+            //吸い込み位置の描画
+            g.DrawEllipse(Pens.Black, Square(magBar.Left, 50));
+
+            //魂の描画
+            foreach (var group in soulGroups)
             {
-                g.FillEllipse(Brushes.Black, Square(p.pos));
+                var brush = new SolidBrush(group.color);
+
+                foreach (var p in group.particles)
+                {
+                    g.FillEllipse(brush, Square(p.pos));
+                }
             }
+
+            //iPhone画面の描画
+            g.DrawRectangle(Pens.Black, windowRect);
         }
 
         void UpdateTick()
         {
-            particles.RemoveAll((Particle p) => { return p.Reach(magBar.Left); });
-            foreach(var p in particles)
+            string num = "";
+            foreach (var group in soulGroups)
             {
-                var v = magBar.GetForce(p);
-                p.Update(v);
+                group.particles.RemoveAll((Soul p) => { return p.Reach(magBar.Left); });
+                foreach (var p in group.particles)
+                {
+                    var v = magBar.GetForce(p);
+                    p.Update(v);
+                }
+                num += group.particles.Count.ToString() + "\n";
             }
+            soulGroups.RemoveAll((SoulGroup l) => { return l.particles.Count == 0; });
 
-            particleNumLabel.Text = particles.Count.ToString();
-            Simulating = particles.Count > 0;
+            particleNumLabel.Text = num;
+
+
+            if (allCheck)
+                AllDispersal();
+
+            UnderSimulating = soulGroups.Count > 0;
+
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (!Simulating)
+            if (!UnderSimulating)
                 return;
 
             UpdateTick();
-            Invalidate();
-        }
-
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (Simulating)
-                return;
-
-            particles.Add(new Particle(e.Location, e.X < magBar.pos.X));
-            Invalidate();
-        }
-
-        private void startToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Simulating = true;
+            Canvas.Invalidate();
         }
 
         private void randomPlacementToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (Simulating)
-                return;
-
             Random r = new Random();
-            foreach(var i in Enumerable.Range(0, 100))
+
+            List<Soul> particles = new List<Soul>();
+            var num = r.Next((int)( maxKill.Value - minKill.Value)) + (int)minKill.Value;
+            foreach(var i in Enumerable.Range(0, num))
             {
 
-                var x = r.Next(this.Width);
-                var y = r.Next(this.Height);
+                var x = r.Next(windowRect.Width) + windowRect.X;
+                var y = r.Next(windowRect.Height) + windowRect.Y;
 
-                particles.Add(new Particle(new PointF(x, y), x < magBar.pos.X));
+                particles.Add(new Soul(new PointF(x, y), (float)soulSpeed.Value));
             }
 
-            Invalidate();
+            soulGroups.Add( new SoulGroup(particles));
+
+            UnderSimulating = true;
+
         }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            soulGroups.Clear();
+        }
+
+        private void soulAccel_ValueChanged(object sender, EventArgs e)
+        {
+            Soul.accel = (float)soulAccel.Value;
+        }
+
+        private void Canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            List<Soul> particles = new List<Soul>();
+            particles.Add(new Soul(e.Location, (float)soulSpeed.Value));
+            soulGroups.Add(new SoulGroup(particles));
+
+            UnderSimulating = true;
+        }
+
+        bool allCheck = false;
+        int nowWidth = 0;
+        void AllDispersal()
+        {
+            if( soulGroups.Count == 0 )
+            {
+                int row = 10;
+                List<Soul> souls = new List<Soul>();
+                foreach (var i in Enumerable.Range(0, row))
+                {
+
+
+                    foreach (var j in Enumerable.Range(0, windowRect.Height))
+                    {
+                        souls.Add(new Soul(new PointF(i + nowWidth + windowRect.X, j + windowRect.Y), (float)soulSpeed.Value));
+                    }
+                }
+                soulGroups.Add(new SoulGroup(souls));
+                nowWidth+=row;
+            }
+        }
+
+        private void allDispersalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //allCheck = true;            
+            //underSimulating = true;       
+        }
+
+        private void minKill_ValueChanged(object sender, EventArgs e)
+        {
+            if (minKill.Value > maxKill.Value)
+                maxKill.Value = minKill.Value;
+        }
+
+        private void maxKill_ValueChanged(object sender, EventArgs e)
+        {
+            if (minKill.Value > maxKill.Value)
+                minKill.Value = maxKill.Value;
+        }
+
+
     }
 }
